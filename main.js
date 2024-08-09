009bfaa12ffac50aca9b22d2b6ac5be6fbd5138f
Martooooooo <tinchobiosca.0@gmail.com>
2024-08-09 14:21:22 -0300

    fafa

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function click() {
-    document.getElementById("contadordemusculo").innerHTML = 
-    Number(document.getElementById("contadordemusculo").innerHTML) + 1;
+
     document.getElementById("contadordecalorias").innerHTML = 
     Number(document.getElementById("contadordecalorias").innerHTML) + 1;
   }
