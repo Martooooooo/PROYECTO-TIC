80a420adb66d04a49f343fbc670348643004f0c4
Teo50000 <167240471+Teo50000@users.noreply.github.com>
2024-08-13 14:30:34 -0300

    jafhafhfgg

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ function click() {
     Number(document.getElementById("contadordecalorias").innerHTML) + 1;
   }
 document.getElementById("circulo").addEventListener("click", click);
+function toggleMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Cerrar el menú si el usuario hace clic fuera de él
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
 
   
   
