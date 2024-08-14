ca6ab2b839dcfb489faabe3169124a34c323ac5a
Martooooooo <tinchobiosca.0@gmail.com>
2024-08-14 09:36:55 -0300

    dfg

--- main.js
+++ main.js
@@ -9,18 +9,7 @@ function toggleMenu() {
 }
 
 
-// Cerrar el menú si el usuario hace clic fuera de él
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      for (var i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-              openDropdown.classList.remove('show');
-          }
-      }
-  }
-}
+
 
   
   
