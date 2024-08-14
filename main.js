2ade6e661cee75a006f125409d26fcf0b46dacfb
Martooooooo <tinchobiosca.0@gmail.com>
2024-08-14 14:47:16 -0300

    asd

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-function click() {
+let contadordecalorias = Number(document.getElementById("contadordecalorias").innerHTML);
 
-    document.getElementById("contadordecalorias").innerHTML = 
-    Number(document.getElementById("contadordecalorias").innerHTML) + 1;
-  }
+function click() {
+  contadordecalorias += 1;
+  document.getElementById("contadordecalorias").innerHTML = contadordecalorias;
+}
+
 document.getElementById("circulo").addEventListener("click", click);
 function toggleMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-
-
+let preciodelcosoeste = 5
+let cantidaddeejercicios = 0
+document.getElementById("ejercicios").addEventListener("click", () => {
+  if (contadordecalorias >= preciodelcosoeste){
+    alert('lo compraste gordo puto')
+    contadordecalorias = contadordecalorias - preciodelcosoeste
+    document.getElementById("contadordecalorias").innerHTML = contadordecalorias;
+    preciodelcosoeste = preciodelcosoeste*2
+    cantidaddeejercicios = cantidaddeejercicios + 1
+    console.log(cantidaddeejercicios)
+  } else alert(`te faltan ${contadordecalorias - preciodelcosoeste}`)
+});
 
   
   
