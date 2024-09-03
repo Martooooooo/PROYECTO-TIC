b0ae6aa0f6c730ef4a79280495961669d1cc0fc9
Martooooooo <tinchobiosca.0@gmail.com>
2024-09-03 08:29:38 -0300

    agrego cosas a base de datos

--- BaseDeDatos.js/BaseDeDatoss.js
+++ BaseDeDatos.js/BaseDeDatoss.js
@@ -7,6 +7,11 @@ onEvent("contadordecalorias",(data)=>{
     let datosjugador= JSON.parse(fs.readFileSync("datos.json", "utf-8 "));
     let nuevo=datosjugador[0].calorias=caloriasactuales;
     fs.writeFileSync("datos.json",JSON.stringify(nuevo,null,2));  
-
+})
+onEvent("contadordedinero",(data)=>{
+    
+    let datosjugador= JSON.parse(fs.readFileSync("datos.json", "utf-8 "));
+    let nuevo=datosjugador[0].dinero=dineroactual;
+    fs.writeFileSync("datos.json",JSON.stringify(nuevo,null,2));  
 })
 startServer();
