aed828cc3fae1879117595f7678c25860003d0c7
Martooooooo <tinchobiosca.0@gmail.com>
2024-09-25 10:19:30 -0300

    nada

--- BaseDeDatos.js/BaseDeDatoss.js
+++ BaseDeDatos.js/BaseDeDatoss.js
@@ -32,12 +32,6 @@ onEvent("comidas", (data) => {
     writeFileSync("datos.json", JSON.stringify(datosjugador, null, 2));
 });
 
-onEvent("ejercicios", (data) => {
-    console.log(data);
-    let datosjugador = JSON.parse(readFileSync("datos.json", "utf-8"));
-    datosjugador[0].ejercicios = data.ejerciciosactuales; 
-    writeFileSync("datos.json", JSON.stringify(datosjugador, null, 2));
-});
 
 onEvent("cantidades", (data) => {
     console.log(data);
